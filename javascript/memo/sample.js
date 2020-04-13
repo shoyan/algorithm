2255095f424db165ab71cb6ab79711ba743c7ad7
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-04-13 21:14:01 +0900

    update

--- javascript/memo/sample.js
+++ javascript/memo/sample.js
@@ -22,7 +22,8 @@ let sampleData = [
 
 // 包括関係順にソートする関数
 // 挿入ソートアルゴリズムで実装
-function sort(data) {
+function sort(_data) {
+  let data = Object.assign([], _data) 
   //未整列の部分から値を１つずつ取り出すfor文
   for (var i = 1; i < data.length; i++) {
     //「挿入する値」を変数に一時保存する
@@ -30,8 +31,8 @@ function sort(data) {
 
     //「整列済みの部分」のどこに挿入すれば良いか後ろから前に向かって順番に判断する
     for (var j = i - 1; j >= 0; j--) {
-      //「挿入する値」が小さい場合、調べた値を１つ後ろへずらす
-      if(data[j].start <= tmp.start && data[j].end >= tmp.end){
+      //「挿入する値」が大きい場合、調べた値を１つ後ろへずらす
+      if(data[j].start >= tmp.start){
         data[j + 1] = data[j]
       } else {
         //小さくなければ、ずらす処理を止める
@@ -41,7 +42,25 @@ function sort(data) {
     //ずらす処理が終わったところに「挿入する値」を入れる
     data[j + 1] = tmp;
   }
-  return data.reverse()
+
+  for (var i = 1; i < data.length; i++) {
+    //「挿入する値」を変数に一時保存する
+    let tmp = data[i];
+
+    //「整列済みの部分」のどこに挿入すれば良いか後ろから前に向かって順番に判断する
+    for (var j = i - 1; j >= 0; j--) {
+      //「挿入する値」が大きい場合、調べた値を１つ後ろへずらす
+      if(data[j].start === tmp.start && data[j].end <= tmp.end){
+        data[j + 1] = data[j]
+      } else {
+        //小さくなければ、ずらす処理を止める
+        break;
+      }
+    }
+    //ずらす処理が終わったところに「挿入する値」を入れる
+    data[j + 1] = tmp;
+  }
+  return data
 }
 
 function getData() {
